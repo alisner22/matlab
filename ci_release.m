669f5141aebadbc64171f22b587995a984829c38
alisner22 <alisner@mathworks.com>
2025-12-12 11:59:23 -0500

    Update ci_release.m

--- ci_release.m
+++ ci_release.m
@@ -1,9 +1,6 @@
-% mpmcreate("mpackage",pwd);
-% pkg = matlab.mpm.Package(pwd);
-% v = pkg.Version;
-% pkg.Version = matlab.mpm.Version(v.Major, v.Minor, v.Patch + 1);
+mpmcreate("mpackage",pwd);
+pkg = matlab.mpm.Package(pwd);
+v = pkg.Version;
+pkg.Version = matlab.mpm.Version(v.Major, v.Minor, v.Patch + 1);
 
-parfor i = 1:3
-    fprintf("hi");
-end
 
